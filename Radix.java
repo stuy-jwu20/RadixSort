e7e659b181e089fa3f0e7a2d8910ff730421089c
Jonathan Wu <jwu20@stuy.edu>
2021-01-12 12:22:35 -0500

    put code int int method

--- Radix.java
+++ Radix.java
@@ -1,16 +1,22 @@
 public class Radix {
-  public static int nth(int n, int col) {
-
-  }
+  // public static int nth(int n, int col) {
+  //
+  // }
 
   public static int length(int n) {
-
-  }
-
-  public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
-    for (int i = 0; i < buckets.length; i++) {
-      original.extend(buckets[i]);
+    int theNumber = Math.abs(n);
+    int digits = 0;
+    while (theNumber > 0) {
+      digits++;
+      theNumber /= 10;
     }
+    return digits;
   }
-  
+
+  // public static void merge(MyLinkedList original, MyLinkedList[] buckets) {
+  //   for (int i = 0; i < buckets.length; i++) {
+  //     original.extend(buckets[i]);
+  //   }
+  // }
+
 }
