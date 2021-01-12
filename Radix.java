8c022b1a8ffca3c3b438308e68d66502bd11ef57
Jonathan Wu <jwu20@stuy.edu>
2021-01-12 15:44:03 -0500

    shortened the code in length method

--- Radix.java
+++ Radix.java
@@ -5,11 +5,8 @@ public class Radix {
   }
 
   public static int length(int n) {
-    if (n == 0) {
-      return 1;
-    } else if (n < 0) {
-      n *= -1;
-    }
+    if (n == 0) return 1;
+    if (n < 0) n *= -1;
     return (int)(Math.log10(n) + 1);
   }
 
