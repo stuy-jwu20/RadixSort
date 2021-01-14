c3046ed9784de708610e4dbbf395dae49236bcc8
Jonathan Wu <jwu20@stuy.edu>
2021-01-14 00:56:59 -0500

    optimized code

--- Radix.java
+++ Radix.java
@@ -1,13 +1,11 @@
 public class Radix {
   public static int nth(int n, int col) {
-    int toDivide = (int)Math.pow(10, col + 1);
-    return Math.abs((n % toDivide) / (toDivide / 10));
+    return (int)(n/Math.pow(10, col)) % 10;
   }
 
   public static int length(int n) {
     if (n == 0) return 1;
-    if (n < 0) n *= -1;
-    return (int)(Math.log10(n) + 1);
+    return (int)Math.log10(Math.abs(n)) + 1;
   }
 
   public static void merge(SortableLinkedList original, SortableLinkedList[] buckets) {
@@ -17,47 +15,46 @@ public class Radix {
   }
 
   public static void radixSortSimple(SortableLinkedList data) {
-    int highestLength = 1;
     SortableLinkedList[] buckets = new SortableLinkedList[10];
     for (int i = 0; i < buckets.length; i++) {
       buckets[i] = new SortableLinkedList();
     }
-    for (int i = 0; i < highestLength; i++) {
+    int digitNum = 0;
+    boolean yes = true;
+    while (yes) {
+      yes = false;
       while (data.size() > 0) {
-        if ((i == 0) && (Radix.length(data.get(0)) > highestLength)) {
-          highestLength = Radix.length(data.get(0));
+        int currentNum = data.remove(0);
+        int digitEnd = nth(currentNum, digitNum);
+        buckets[digitEnd].add(currentNum);
+        if (Radix.length(currentNum) > digitNum + 1) {
+          yes = true;
         }
-        int digitPlace = Radix.nth(data.get(0), i);
-        buckets[digitPlace].add(data.get(0));
-        data.remove(0);
       }
-      Radix.merge(data, buckets);
+      digitNum++;
+      merge(data, buckets);
     }
   }
 
   public static void radixSort(SortableLinkedList data) {
-    SortableLinkedList negNum = new SortableLinkedList();
-    SortableLinkedList posNum = new SortableLinkedList();
-    SortableLinkedList zero = new SortableLinkedList();
-    for (int i = 0; i < data.size(); i++) {
-      int number = data.get(i);
-      if (number > 0) {
-        posNum.add(number);
-      } else if (number < 0) {
-        negNum.add(number * -1);
-      } else {
-        zero.add(number);
+    SortableLinkedList[] buckets = new SortableLinkedList[19];
+    for (int i = 0; i <= 18; i++) {
+      buckets[i] = new SortableLinkedList();
+    }
+    int digitNum = 0;
+    boolean yes = true;
+    while (yes) {
+      yes = false;
+      while (data.size() > 0) {
+        int currentNum = data.remove(0);
+        int digitEnd = nth(currentNum, digitNum)+9;
+        buckets[digitEnd].add(currentNum);
+        if (Radix.length(currentNum) > digitNum + 1) {
+          yes = true;
+        }
       }
+      digitNum++;
+      merge(data, buckets);
     }
-    Radix.radixSortSimple(negNum);
-    Radix.radixSortSimple(posNum);
-    for (int i = 0; i < data.size(); i++) {
-      data.remove(i);
-    }
-    for (int i = negNum.size()-1; i >= 0; i--) {
-      data.add(negNum.get(i) * -1);
-    }
-    data.extend(zero);
-    data.extend(posNum);
   }
 }
